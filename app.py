0e9a1ffb34d6d285cfc0a1c11e25b74d6be5c614
Bimal Patra <134154083+bimal-bp@users.noreply.github.com>
2025-03-26 17:10:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def show_story_page():
         Hi, my name is **Bimal Patra**, and I work as a **Data Scientist at SG Group in Mumbai**.  
         -  I'm sharing my story not because I was a topper or someone from IIT/IIM college 
         - and earning in lakhs or have any business and owend a company ,
+
         but because I made many mistakes in my educational choices.  
         I hope my experience helps you make better decisions.
     """, unsafe_allow_html=True)
